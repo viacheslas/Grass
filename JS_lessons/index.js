740f5d4076fdd354c1e9b3bc0186086cafbc3f73
viacheslas <krachmer@gmail.com>
2017-08-13 22:32:48 +0300

    lip at home

---
+++ JS_lessons/index.js
@@ -0,0 +1,55 @@
+/**
+ * Created by viacheslas on 10.08.17.
+ */
+
+
+function myclick () {
+
+
+    let testfunction = function () {
+        return 22;
+    };
+
+    console.log(6);
+    //
+    // let myArray = [1, 2, 3];
+    // console.log(myArray);
+    //
+    // let myObject = {
+    //     height: 200,
+    //     width: 500,
+    //     color: 'red'
+    // };
+    // console.log(myObject);
+    // console.log(testfunction());
+
+    (function(){
+        console.log('unanimous');
+    })();
+}
+
+
+
+
+
+
+
+
+
+
+   /**
+    var -legacy
+    const
+    let
+    object{}
+    array[]
+
+    string
+    number
+    boolean
+    null
+    undefined
+    type.__proto__  --> type methods
+
+    */
+
